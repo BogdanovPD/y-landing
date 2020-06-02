fd5b4f1469cccdf7e6a82d2b9994cc8e6f16c121
bogdanovpd@gmail.com <BogdanovPD>
2020-06-02 18:27:14 +0300

    fix margin lr

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
   jQuery(window).load(function() {
     jQuery("#preloader").delay(1).fadeOut("slow");
     jQuery("#load").delay(1).fadeOut("slow");
-    $('#intro').fadeOut(3000);
+    $('#intro').fadeOut(2000);
   });
 
 
